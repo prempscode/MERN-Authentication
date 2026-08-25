f2f17d7b12970786912979099de398df6e71bd45
premdev <prempravash65@gmail.com>
2026-08-26 00:46:56 +0530

    Update auth.route.js

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register, login } from '../controllers/auth.controller.js'
+import { register, login, logout } from '../controllers/auth.controller.js'
 import protectedRoute from '../middleware/protected.middleware.js'
 
 const router = express.Router()
@@ -9,12 +9,9 @@ router.post('/login', login)
 router.post('/logout', logout)
 
 router.get('/', protectedRoute, (req, res) => {
-  try {
-    res.json({
-      message: 'user authorized'
-    })
-  } catch (err) {
-    console.log(err.message)
-  }
+  return res.json({
+    message: 'User authorized',
+    userId: req.userId
+  })
 })
 export default router
