b2f6566c3a54bf0f76467ec81820968ef363e9bd
premdev <prempravash65@gmail.com>
2026-08-25 23:59:08 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,16 @@
 import userModel from '../models/user.model'
 
-const register = (req, res) => {}
+const register = (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    if (!username || !email || !password) {
+      res.status(400).json({
+        message: 'All filled required'
+      })
+    }
+  } catch (err) {
+    console.log('Register Controller error : ', err.message)
+  }
+}
 
 export default { register }
